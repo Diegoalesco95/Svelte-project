7dcef75921547010d7b14e5986786989f0b1202e
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-06-09 12:28:42 -0500

    Styles

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,3 +1,28 @@
+<style>
+  .Profile-content {
+    display: flex;
+    align-items: center;
+  }
+  .Profile-avatar img {
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+  }
+  .Profile-info {
+    margin: 0 0 0 0.5em;
+  }
+  .Profile-info h2 {
+    font-size: 14px;
+    color: black;
+    margin: 0;
+    padding: 0;
+  }
+  .Profile-info span {
+    font-size: 12px;
+    font-weight: normal;
+  }
+</style>
+
 <div class="Profile">
   <div class="Profile-content">
     <div class="Profile-avatar">
@@ -5,9 +30,9 @@
         src="https://es.gravatar.com/userimage/104707076/91c6e3674aede502614de01e1357a5c0.jpg?size=200"
         alt="" />
     </div>
-  </div>
-  <div class="Profile-info">
-    <h2>Diegoalesco95</h2>
-    <span>Diego Osorio</span>
+    <div class="Profile-info">
+      <h2>Diegoalesco95</h2>
+      <span>Diego Osorio</span>
+    </div>
   </div>
 </div>
