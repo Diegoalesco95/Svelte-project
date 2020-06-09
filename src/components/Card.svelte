dc3521edcfa6562553942fba62418c8bf4bd6477
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-06-09 11:01:37 -0500

    Timeline

---
+++ src/components/Card.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Comments from "./Comments.svelte";
+</script>
+
+<div class="Card">
+  <div class="Card-container">
+    <div class="Card-header">
+      <div class="Card-user">
+        <img
+          src="https://images.dog.ceo/breeds/pug/n02110958_11732.jpg"
+          alt="" />
+        <h2>
+          Evangeline.pug
+          <span>North Brian, Bedfordshire</span>
+        </h2>
+      </div>
+      <div class="Card-settings">
+        <i class="fas fa-ellipsis-h" />
+      </div>
+    </div>
+    <div class="Card-photo">
+      <figure>
+        <img
+          src="https://images.dog.ceo/breeds/pug/n02110958_11732.jpg"
+          alt="" />
+      </figure>
+    </div>
+    <div class="Card-icons">
+      <div class="Card-icons-first">
+        <i class="fas fa-heart" />
+        <i class="fas fa-paper-plane" />
+      </div>
+      <div class="Card-icons-second">
+        <i class="fas fa-bookmark" />
+      </div>
+    </div>
+    <div class="Card-description">
+      <h3>Evangeline.pug</h3>
+      <span>Hola!</span>
+    </div>
+    <Comments />
+  </div>
+</div>
---
+++ src/components/Comments.svelte
@@ -0,0 +1,18 @@
+<div class="Comments">
+  <div class="Comments-content">
+    <div class="Comments-users">
+      <h3>Delpha</h3>
+      <span>Hola Evangeline</span>
+    </div>
+    <div class="Comments-add">
+      <form action="">
+        <input
+          type="text"
+          class="Comments-input"
+          placeholder="Agregar comentario..."
+          id="text" />
+        <button type="submit">Post</button>
+      </form>
+    </div>
+  </div>
+</div>
